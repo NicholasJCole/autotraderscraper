22dae50774b4d6b3b6f2783720b8511d71e11b9f
Nicholas Cole <nicholasjcole1@gmail.com>
2014-01-27 23:10:35 -0600

    added time, worked on database'

--- atdata.py
+++ atdata.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import urllib2
 import re
 import mechanize
+import time
+import sqlite3
+
+time = time.ctime()
 
 url = 'http://www.autotrader.com/cars-for-sale/Used+Cars/Toyota/Camry/Dallas+TX-75204?endYear=2015&listingType=used&listingTypes=used&makeCode1=TOYOTA&mmt=%5BTOYOTA%5BCAMRY%5B%5D%5D%5B%5D%5D&modelCode1=CAMRY&searchRadius=75&showcaseListingId=362401789&showcaseOwnerId=100025948&sortBy=derivedpriceASC&startYear=2008&Log=0'
 
@@ -24,7 +28,6 @@ car_data = BeautifulSoup(html)
 #get make and model
 #make_model = re.findall(r'>(Used[a-zA-Z0-9 ]+)', str(data))
 
-
 paragraph_data = car_data.find_all('div', {'class':'listing listing-findcar listing-dealer '})
 
 make_models = []
@@ -44,9 +47,22 @@ for section in paragraph_data:
 cleaned_mm_prices = []
 for eachline in mm_prices:
 	cleaned_mm_prices.append(eachline.replace(",", "").replace("$", ""))
-
-   
+ 
+#"+yy to copy a line
 
 #make_model = re.findall(r'>(Used[a-zA-Z0-9 ]+)', str(paragraph_data))
 
-print(cleaned_mm_prices)
+db = sqlite3.connect('Documents/Python/autotrader/atdatabase')
+cursor = db.cursor()
+# used, year, brand, make, trim, price, mileage, time
+cursor.execute('create table atdata(used TEXT, year INTEGER, brand TEXT, make TEXT, trim TEXT, price REAL, mileage REAL, time TEXT)')
+
+# db.execute('insert into atdata(used, year, brand, make, trim, price, mileage, time), (
+
+split_lines = []
+for i in cleaned_mm_prices:
+    data = i.split(" ", 6)
+    db.execute('insert into atdata(used, year, brand, make, trim, price, mileage, time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', (data[0], data[1], data[2], data[3], data[4], data[5], data[6], time))
+
+cursor.execute('select * from atdata')
+cursor.fetchall()
